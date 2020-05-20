42e92a8ff989e8efdb04de62ab334838ca579e18
wltiii <github.0vx@gishpuppy.com>
2020-05-20 08:23:38 -0500

    BT - added fromJson constructor

--- lib/src/domain/expectation-template.dart
+++ lib/src/domain/expectation-template.dart
@@ -1,24 +1,37 @@
+import 'dart:convert';
+
 class ExpectationTemplate {
   Duration allottedDuration;
-  final String activityTitle;
-  final String activityHint;
-  final List reflectionQuestions;
+  String activityTitle;
+  String activityHint;
+  List reflectionQuestions;
 
-  ExpectationTemplate({
+  ExpectationTemplate(
     this.activityTitle,
     this.activityHint,
     this.allottedDuration,
     this.reflectionQuestions
-  });
+  );
+
+  ExpectationTemplate.fromJson(String json) {
+    Map decoded = jsonDecode(json);
+    Duration d = Duration(minutes: decoded['allottedDuration']);
+    this.allottedDuration = d;
+    this.activityTitle = decoded['activityTitle'];
+    this.activityHint = decoded['activityHint'];
+    this.reflectionQuestions = decoded['reflectionQuestions'];
+  }
 }
 
-final activity = new ExpectationTemplate(
-  allottedDuration: Duration(minutes: 10),
-  activityTitle: 'What do you plan to accomplish during this session?',
-  activityHint: 'Enter your intention.',
-  reflectionQuestions: [
-      'What happened during the allotted time?',
-      'What explains the difference?',
-      'What action can you take to improve outcomes?'
-    ]
-);
+
+
+final activity = ExpectationTemplate.fromJson('''{
+  "allottedDuration": 10,
+  "activityTitle": "What do you plan to accomplish during this session?",
+  "activityHint": "Enter your intention.",
+  "reflectionQuestions": [
+    "What happened during the allotted time?",
+    "What explains the difference?",
+    "What action can you take to improve outcomes?"
+  ]
+}''');
