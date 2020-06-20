58d9cfa458984bcae132d388f6adf8f74f983488
wltiii <github.0vx@gishpuppy.com>
2020-06-20 09:03:24 -0500

    [35] BT - Improved UX of action timer. A number of linting changes.

--- lib/src/action_timer_widget.dart
+++ lib/src/action_timer_widget.dart
@@ -2,14 +2,11 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import 'domain/question_answer.dart';
 import 'domain/reflection.dart';
 import 'reflection_widget.dart';
 
-// stopwatch page
 // shows progress bar with time remaining
 // alarm when time ends
-// silence alarm and continue (encourages wrong behavior - think carefully)
 
 class ActionTimerWidget extends StatefulWidget {
   static const routeName = '/action-timer';
@@ -17,8 +14,7 @@ class ActionTimerWidget extends StatefulWidget {
     Key key,
     this.title,
   }) : super(key: key);
-
-
+  
   final String title;
   _ActionTimer createState() => _ActionTimer();
 }
@@ -27,34 +23,35 @@ class _ActionTimer extends State<ActionTimerWidget> {
   bool _initialized = false;
   Timer _timer;
   Duration _timeRemaining;
-  Duration timeout = const Duration(seconds: 1);
+  final Duration _timeout = const Duration(seconds: 1);
   Icon pauseIcon = const Icon(Icons.pause_circle_outline);
   Icon playIcon = const Icon(Icons.play_circle_outline);
   Icon _pausePlayIcon;
   String _pausePlayTooltip;
-  // TODO: these two functions look like they will be used in multiple locations. Consider moving to another class.
-  toHHMMSS(Duration d) => d.toString().split('.').first.padLeft(8, "0");
-  toMMSS(Duration d) => d.toString().substring(2, 7);
+  // TODO: these two functions look like they will be used in multiple 
+  //  locations. Consider moving to another class.
+  _toHHMMSS(Duration d) => d.toString().split('.').first.padLeft(8, "0");
+  _toMMSS(Duration d) => d.toString().substring(2, 7);
 
   @override
   void initState() {
-    startTimer();
+    _startTimer();
     _pausePlayIcon = pauseIcon;
     _pausePlayTooltip = 'Pause';
     super.initState();
   }
 
-  startTimer() => _timer = Timer.periodic(timeout, handleTimerEvent);
+  _startTimer() => _timer = Timer.periodic(_timeout, handleTimerEvent);
 
-  void stopTimer() => _timer.cancel();
+  _stopTimer() => _timer.cancel();
 
   void handleTimerEvent(Timer timer) {
     setState(() {
-      _timeRemaining = _timeRemaining - timeout;
+      _timeRemaining = _timeRemaining - _timeout;
       if (_timeRemaining.inMilliseconds == 0) {
         print(
           'ALERT NOW - visual and audio and tactile (configurable)');
-        stopTimer();
+        _stopTimer();
       }
     });
   }
@@ -62,11 +59,11 @@ class _ActionTimer extends State<ActionTimerWidget> {
   void swapPausePlayButton() {
     setState(() {
       if (_pausePlayIcon == pauseIcon) {
-        stopTimer();
+        _stopTimer();
         _pausePlayIcon = playIcon;
         _pausePlayTooltip = 'Continue';
       } else {
-        startTimer();
+        _startTimer();
         _pausePlayIcon = pauseIcon;
         _pausePlayTooltip = 'Pause';
       }
@@ -75,10 +72,10 @@ class _ActionTimer extends State<ActionTimerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    final Reflection args = ModalRoute.of(context).settings.arguments;
+    final Reflection _args = ModalRoute.of(context).settings.arguments;
 
-    // TODO this seems wrong. How can it be initialized from args without this method?
-    initFromArgs(args);
+    // TODO this seems wrong. Can it be initialized without this method?
+    _initFromArgs(_args);
 
     return Scaffold(
       appBar: AppBar(
@@ -86,31 +83,47 @@ class _ActionTimer extends State<ActionTimerWidget> {
       ),
       body: Center(
           child: Column(children: <Widget>[
-            Text(args.expectation.question),
-            Text('Time remaining ${toHHMMSS(_timeRemaining)}.'),
-        // TODO add progress bar
-        // TODO play alert sound when time is up (and show timer going negative?)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                _args.expectation.question,
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.normal),
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text(
+                _args.expectation.answer,
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Time remaining ${_toHHMMSS(_timeRemaining)}.',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.normal),
+              ),
+            ),
+            // TODO add progress bar
+        // TODO play alert when time is up (and show timer going negative?)
         Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
           IconButton(
             padding: const EdgeInsets.only(),
             icon: _pausePlayIcon,
             tooltip: _pausePlayTooltip,
-            onPressed: () {
-              // TODO pause timer
-              swapPausePlayButton();
-            },
+            onPressed: swapPausePlayButton,
           ),
           IconButton(
             padding: const EdgeInsets.only(),
             icon: Icon(Icons.stop),
             tooltip: 'Stop',
             onPressed: () {
-              stopTimer();
-                  args.actualDurationSeconds = _timeRemaining;
+              _stopTimer();
+                  _args.actualDurationSeconds = _timeRemaining;
               Navigator.pushNamed(
                   context,
                   ReflectionWidget.routeName,
-                  arguments: args,
+                  arguments: _args,
               );
             },
           ),
@@ -119,7 +132,7 @@ class _ActionTimer extends State<ActionTimerWidget> {
     );
   }
 
-  initFromArgs(Reflection args) {
+  _initFromArgs(Reflection args) {
     if (!_initialized) {
       _timeRemaining = args.allottedDuration;
       _initialized = true;
