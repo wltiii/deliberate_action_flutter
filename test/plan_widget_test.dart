3e66fba8bb8782605d8c3412b433a35b36e08c20
wltiii <github.0vx@gishpuppy.com>
2020-06-23 08:49:47 -0500

    [22] BT - Added more widget testing of PlanWidget.

--- test/plan_widget_test.dart
+++ test/plan_widget_test.dart
@@ -24,29 +24,43 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
         makeTestableWidget(
-            PlanWidget(title: 'Test title')
+            PlanWidget(title: 'Plan Widget')
         )
     );
 
     // create finders
-    final titleFinder = find.text('Test title');
+    final titleFinder = find.text('Plan Widget');
+    final durationFinder = find.text(
+        'Time allotted to complete action is 00:10:00.');
+    final addButtonFinder = find.byIcon(Icons.add_circle_outline);
+    final subtractButtonFinder = find.byIcon(Icons.remove_circle_outline);
 
     // rebuild widget
 //    await tester.pumpAndSettle();
 
     // verify text widgets appear once
     expect(titleFinder, findsOneWidget);
+    expect(durationFinder, findsOneWidget);
 
-//    // Verify that our counter starts at 0.
-//    expect(find.text('0'), findsOneWidget);
-//    expect(find.text('1'), findsNothing);
-//
 //    // Tap the '+' icon and trigger a frame.
-//    await tester.tap(find.byIcon(Icons.add));
-//    await tester.pump();
-//
+    await tester.tap(addButtonFinder);
+    await tester.pump();
 //    // Verify that our counter has incremented.
-//    expect(find.text('0'), findsNothing);
-//    expect(find.text('1'), findsOneWidget);
+    expect(
+        find.text('Time allotted to complete action is 00:11:00.'),
+        findsOneWidget,
+    );
+//    // Tap the '-' icon and trigger a frame.
+    await tester.tap(subtractButtonFinder);
+    await tester.tap(subtractButtonFinder);
+    await tester.pump();
+    expect(
+        find.text('Time allotted to complete action is 00:09:00.'),
+        findsOneWidget,
+    );
+
+    //TODO add test to verify entry requirement
+    //TODO add test to verify transition to new screen
+    // Can it validate the arguments passed are as expected? should be able to.
   });
 }
